fa8a117b4b30b0169b91330fea412327d154b6f4
Raven <yakubsiq@gmail.com>
2023-08-10 17:06:28 +0200

    Got rid of useless strings in assembly func

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,12 @@
 
 #include "headers/parser.hpp"
 #include "headers/code.hpp"
-#include "headers/labels.hpp"
+#include "headers/dymanicLabelVars.hpp"
 
 bool isAinstruction(const std::string& instruction);
 bool isCinstruction(const std::string& instruction);
 bool isLabel(const std::string& instruction);
+bool isVar(const std::string& instruction);
 int toInt(const std::string& str);
 void processFile(std::ifstream& assembly,const std::string& hackName);
 void cleanFile(std::ifstream& assembly,const std::string& tempFileName);
@@ -23,7 +24,6 @@ std::string removeSpaces(const std::string& memoryBuffer);
 
 int main(int argc, char* argv[]){
     try{
-        std::string instruction;
         std::string assemblyName = argv[1];
         std::ifstream assembly(assemblyName);
         if(!assembly.good()){
@@ -68,6 +68,13 @@ bool isLabel(const std::string& instruction){
     }
     return false;
 }
+bool isVar(const std::string& instruction){
+    if(instruction.at(0) == '@' && !(isAinstruction(instruction)) &&
+        dyn_map.label_map.find(instruction.substr(1, instruction.length())) ==
+        dyn_map.label_map.end())
+            return true;
+    return false;
+}
 int toInt(const std::string& str){
     std::istringstream iss(str);
     int result;
@@ -90,25 +97,42 @@ void processFile(std::ifstream& assembly, const std::string& hackName){
 }
 
 void cleanFile(std::ifstream& assembly,const std::string& hackName){
-    std::ofstream hackFile(hackName);
-    std::string cleanInstruction;
+    std::string* instructionPtr = new std::string();
     unsigned int instructionPosition = 1;
-
-    while(std::getline(assembly, cleanInstruction)){
-        std::string instruction = removeSpaces(cleanInstruction);
+    
+    std::stringstream asssemblyMemoryBuffer;
+    while(std::getline(assembly, *instructionPtr)){
+        std::string instruction = removeSpaces(*instructionPtr);
         if(instruction == "" || instruction.substr(0,2) == "//")
             continue;
         size_t commentLocation = instruction.find("//");
         if(commentLocation != std::string::npos)
             instruction = instruction.substr(0, commentLocation);
         if(isLabel(instruction)){
-            std::string modifiedInstruction = cleanInstruction.substr(1,(cleanInstruction.length()-2));
-            Labels.label_map.insert({modifiedInstruction,instructionPosition-1});
-            //std::cout << modifiedInstruction<<std::endl;
+            std::string modifiedInstruction = instruction.substr(1,(instruction.length()-2));
+            dyn_map.label_map.insert({modifiedInstruction,instructionPosition-1});
+            //std::cout << "Label:" << modifiedInstruction << " at " << instructionPosition-1<<std::endl;
             continue;
         }
-        hackFile << instruction << "\n";
+        asssemblyMemoryBuffer << instruction << "\n";
+        //std::cout << instruction << std::endl;
         instructionPosition++;
+    }
+    delete(instructionPtr);
+    unsigned int varStart = 16;
+    std::string instruction;
+    std::ofstream hackFile(hackName);
+    while(std::getline(asssemblyMemoryBuffer, instruction)){
+        if(isVar(instruction)){
+            std::string modifiedInstruction = instruction.substr(1,instruction.length());
+            if(dyn_map.var_map.find(modifiedInstruction) == dyn_map.var_map.end()){
+                dyn_map.var_map.insert({modifiedInstruction, varStart});
+                //std::cout << modifiedInstruction <<  " at " << varStart << std::endl;
+                varStart++;
+            }
+            //std::cout << instruction << std::endl;
+        }
+        hackFile << instruction << "\n";
     }
     assembly.close();
     hackFile.close();
@@ -122,8 +146,16 @@ void checkLabelVars(const std::string& hackName){
     hackRead.close();
     while(std::getline(hackMemoryBuffer, instruction)){
         if(instruction.at(0) == '@' && !(isAinstruction(instruction))){
-            std::string label = instruction.substr(1, instruction.length());
-            hackFile << "@" << Labels.label_map[label] << "\n";
+            if(dyn_map.label_map.find(instruction.substr(1, instruction.length())) != dyn_map.label_map.end()){
+                hackFile << "@" << dyn_map.label_map[instruction.substr(1, instruction.length())] << "\n";
+                //std::cout << dyn_map.label_map[instruction. substr(1, instruction.length())] << std::endl;
+            }else if(dyn_map.var_map.find(instruction.substr(1, instruction.length())) != dyn_map.var_map.end()){
+                hackFile << "@" << dyn_map.var_map[instruction.substr(1, instruction.length())] << "\n";
+                //std::cout << dyn_map.var_map[instruction.substr(1, instruction.length())] << std::endl;
+            }else{
+               std::cout << "checkLabelVars::while::ifVarOrLabel error" << std::endl;
+            }
+            
             //std::cout << Labels.label_map[label] << std::endl;
             //std::cout  << label << "\n";
             continue;
@@ -131,7 +163,6 @@ void checkLabelVars(const std::string& hackName){
     hackFile << instruction << "\n";
     }
     hackFile.close();
-
 }
 void assemble(const std::string& hackName){
     std::ifstream tempHackRead(hackName);
@@ -151,18 +182,14 @@ void assemble(const std::string& hackName){
         }if(isCinstruction(instruction)){
             Parser parser(instruction);
             Code code;
-            std::string comp = parser.comp();
-            std::string dest = parser.dest();
-            std::string jump = parser.jump();
             
-            std::string compBits = code.comp(comp);
-            std::string destBits = code.dest(dest);
-            std::string jumpBits = code.jump(jump);
+            std::string compBits = code.comp(parser.comp());
+            std::string destBits = code.dest(parser.dest());
+            std::string jumpBits = code.jump(parser.jump());
             //std::cout << destBits << compBits << jumpBits << std::endl;
             hackFile << "111" << compBits  << destBits << jumpBits << "\n";
             continue;
         }
-
         //std::cout << instruction << std::endl;
     }
     hackFile.close();
